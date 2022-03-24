3bec83176dc22f61aa9241c3f50ea482f62d5b23
Macey Jones <89227313+maceyraejones@users.noreply.github.com>
2022-03-24 14:41:13 -0500

    Add files via upload

---
+++ react-frontend/screen/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cards from "../components/Cards"
+import { Link } from 'react-router-dom'
+const Home = () => {
+  return (
+    <div>
+
+<Cards/>
+
+
+    </div>
+  )
+}
+
+export default Home
